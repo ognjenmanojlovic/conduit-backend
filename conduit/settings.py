a3b8ff6c493899844361099c5741f5d629193ef8
Ognjen Manojlovic <ogisha@MacBookPro.local>
2025-12-08 01:00:16 +0100

    feat: configure ALLOWED_HOSTS via environment variable

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
+
 
 
 # Application definition
